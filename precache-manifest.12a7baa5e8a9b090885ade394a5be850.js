8642c4fc77ed3da58c11a721774dd08b3be6949c
detorresramos <detorresramos1@gmail.com>
2019-09-21 15:02:06 -0500

    Updates

---
+++ precache-manifest.12a7baa5e8a9b090885ade394a5be850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdb9a97d3ecfe154e290ebe4abe668c",
+    "url": "/TESTING/index.html"
+  },
+  {
+    "revision": "a9271c9dd3de8341cbb5",
+    "url": "/TESTING/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a1fcedc6299bf2f8e53b",
+    "url": "/TESTING/static/js/2.78659e9d.chunk.js"
+  },
+  {
+    "revision": "a9271c9dd3de8341cbb5",
+    "url": "/TESTING/static/js/main.4a288969.chunk.js"
+  },
+  {
+    "revision": "c1ef503f95cc98b8a25b",
+    "url": "/TESTING/static/js/runtime-main.8f7220f2.js"
+  }
+]);
